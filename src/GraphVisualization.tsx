a2c3c3d4c8a74d9408833b8b09fe20fc678fcaa5
Vadim Sevriuk <vadim.sevriuk@gmail.com>
2024-04-17 15:49:53 -0400

    Refactor GraphVisualization and graph_d3

---
+++ src/GraphVisualization.tsx
@@ -0,0 +1,91 @@
+import React, { Component, RefObject } from 'react';
+import * as d3 from 'd3';
+import { D3GraphPoint, D3Graph } from './algorithms/graph_d3';
+
+
+interface GraphComponentBaseProps<T> {
+    width: number;
+    height: number;
+    graph: D3Graph<T>;
+}
+
+class GraphComponentBase<T> extends Component<GraphComponentBaseProps<T>> {
+    private svgRef: RefObject<SVGSVGElement>;
+
+    constructor(props: GraphComponentBaseProps<T>) {
+        super(props);
+        this.svgRef = React.createRef<SVGSVGElement>();
+    }
+
+    componentDidMount() {
+        this.initializeGraph();
+    }
+
+    componentDidUpdate(prevProps: GraphComponentBaseProps<T>) {
+        if (prevProps.graph !== this.props.graph) {
+            this.initializeGraph();
+        }
+    }
+
+    initializeGraph() {
+        const { width, height, graph } = this.props;
+        const svg = d3.select(this.svgRef.current);
+        svg.selectAll('*').remove();
+        const links = graph.getLinks();
+        const nodes = graph.getNodes();
+
+        const simulation = d3.forceSimulation(nodes)
+            .force('link', d3.forceLink(links).id((d: any) => d.id).strength(this.linkStrength()))
+            .force('charge', d3.forceManyBody().strength(this.chargeStrength()))
+            .force('center', d3.forceCenter(width / 2, height / 2));
+
+        const link = svg.append('g')
+            .selectAll("line")
+            .data(links)
+            .enter().append("line")
+            .style("stroke", "black");
+
+        const node = svg.append('g')
+            .selectAll("circle")
+            .data(nodes)
+            .enter().append("circle")
+            .attr("r", 5);
+
+        simulation.on("tick", () => {
+            link
+                .attr("x1", (d: any) => d.source.x)
+                .attr("y1", (d: any) => d.source.y)
+                .attr("x2", (d: any) => d.target.x)
+                .attr("y2", (d: any) => d.target.y);
+
+            node
+                .attr("cx", (d: any) => d.x)
+                .attr("cy", (d: any) => d.y);
+        });
+    }
+
+    linkStrength(): number {
+        return 0; // Default strength, can be overridden
+    }
+
+    chargeStrength(): number {
+        return -30; // Default strength, can be overridden
+    }
+
+
+    render() {
+        const { width, height } = this.props;
+        return <svg ref={this.svgRef} width={width} height={height} />;
+    }
+}
+
+// Specific component implementations
+export class GraphComponent<T> extends GraphComponentBase<T> {
+    linkStrength() {
+        return 0;
+    }
+
+    chargeStrength() {
+        return 0;
+    }
+}
--- src/algorithms/graph_d3.ts
+++ src/algorithms/graph_d3.ts
@@ -1,8 +1,8 @@
 import { Graph, Edge } from './graph';
 import { Point } from './point';
 
-export class D3GraphAlpha<T> {
-    private graph: Graph<T>;
+export class D3Graph<T> {
+    protected graph: Graph<T>;
 
     constructor(graph: Graph<T>) {
         this.graph = graph;
@@ -29,28 +29,13 @@ export class D3GraphAlpha<T> {
     }
 }
 
-export class D3GraphPoint {
-    private graph: Graph<Point>;
-
-    constructor(graph: Graph<Point>) {
-        this.graph = graph;
-    }
-
+export class D3GraphPoint extends D3Graph<Point> {
     getNodes(): { id: string, x: number, y: number }[] {
         // Generate nodes data for D3, using the x, y directly from Point
         return this.graph.getNodes().map((point) => ({
             id: this.graph.getHashFunction()(point),
             x: point.x,
             y: point.y
-        }));
-    }
-
-    getLinks(): { source: string, target: string, weight: number }[] {
-        // Generate links data for D3
-        return this.graph.getEdges().map(edge => ({
-            source: this.graph.getHashFunction()(edge.origin),
-            target: this.graph.getHashFunction()(edge.destination),
-            weight: edge.weight
         }));
     }
 }
